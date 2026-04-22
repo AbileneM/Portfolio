3db0e7a8fe29394f9c815fa82d2f27b31b4fc6ed
AbileneM <abileneahue@gmail.com>
2026-04-21 21:30:03 -0400

    gestion des erreur page Temoignage

--- store/slices/temoignageSlice.js
+++ store/slices/temoignageSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const testimonialSlice = createSlice({
-  name: "testimonials",
+const temoignageSlice = createSlice({
+  name: "temoignages",
   initialState: {
     items: [],
   },
   reducers: {
-    setTestimonials: (state, action) => {
+    setTemoignages: (state, action) => {
       state.items = action.payload;
     },
   },
 });
 
-export const { setTestimonials } = testimonialSlice.actions;
-export default testimonialSlice.reducer;
+export const { setTemoignages } = temoignageSlice.actions;
+export default temoignageSlice.reducer;
